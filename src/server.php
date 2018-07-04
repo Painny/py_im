890762b6b0011472bd9afb3315a476376a54f904
pengyu <917103429@qq.com>
2018-07-04 17:33:52 +0800

    暂存已登录用户

--- src/server.php
+++ src/server.php
@@ -89,7 +89,7 @@ class Im{
         $this->pushOne($serv,$req->fd,"connected",$response);
 
         //暂存用户实例
-        $user->save();
+        $user->save($serv->redis);
     }
 
     public function onMessage(swoole_server $serv, swoole_websocket_frame $frame)
